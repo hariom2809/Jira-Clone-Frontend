80a3cc5f482161f11c94bdee597433aa2a11611d
Guptahariom28 <hariomgupta2809@gmail.com>
2026-06-05 22:04:19 +0530

    map the issues in their respective columns

--- src/pages/KambanBoard.jsx
+++ src/pages/KambanBoard.jsx
@@ -1,8 +1,32 @@
 import { useState } from "react"
+import { useIssues } from "../hooks/issueHooks"
 
 export default KambanBoard () {
 
-    
+    const projectId = 1234
+
+    const issuesQuery = useIssues(projectId)
+    const issues = issuesQuery.data?.results || []
+
+    if (issuesQuery.isLoading) return <div> Loading ...</div>
+    if (issuesQuery.isError) return <div>  Failed to load issues </div>
+
+    const todoIssues = issues.filter(
+        (issues) => issues.status === "to_do"
+    )
+
+    const inProjgressIssues = issues.filter(
+        (issues) => issues.status === "in_progress"
+    )
+
+    const inReviewIssues = issues.filter(
+        (issues) => issues.status === "in_review"
+    )
+
+    const doneIssues = issues.filter(
+        (issues) => issues.status === "done"
+    )
+
     return(
         <div>
             <h1>Kanabn Board</h1>
@@ -10,15 +34,35 @@ export default KambanBoard () {
             <div>
                 <div>
                     <h2>To Do</h2>
+                    {todoIssues.map((issue) => (
+                        <div key={issue.id}>
+                        {issue.title}
+                        </div>
+                    ))}
                 </div>
                 <div>
                     <h2>In Progress</h2>
+                    {inProjgressIssues.map((issue) => (
+                        <div key={issue.id}>
+                        {issue.title}
+                        </div>
+                    ))}
                 </div>
                 <div>
                     <h2>In Review</h2>
+                    {inReviewIssues.map((issue) => (
+                        <div key={issue.id}>
+                        {issue.title}
+                        </div>
+                    ))}
                 </div>
                 <div>
                     <h2>Done</h2>
+                    {doneIssues.map((issue) => (
+                        <div key={issue.id}>
+                        {issue.title}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
