25f9c7a74b2923b64888b2423003e71bb8b7be24
Guptahariom28 <hariomgupta2809@gmail.com>
2026-06-07 23:40:33 +0530

    card clickable

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,6 +1,9 @@
-export default function Card ({ children, className }) {
+export default function Card ({ children, onClick, className }) {
     return (
-        <div className={`bg-[#1F2937] border border-[#4B5563] rounded-xl p-5 ${className}`}>
+        <div
+            onClick={onClick}
+            className={`bg-[#1F2937] border border-[#4B5563] rounded-xl p-5 ${className}`}
+        >
             {children}
         </div>
     )
