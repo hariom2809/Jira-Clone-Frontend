87bc4c11c3ce5c20c1287e3a70e6912e5150d41b
Guptahariom28 <hariomgupta2809@gmail.com>
2026-06-11 16:47:55 +0530

    refresh token added in interceptor

--- src/app/api.js
+++ src/app/api.js
@@ -5,4 +5,25 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.request.use(
+  (Response) => Response,
+
+  async (error) => {
+    const originalRequest = error.config
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+
+      try {
+        await api.post("/auth/token/refresh")
+        return api(originalRequest)
+      } catch (refreshError) {
+        window.location.href = "/login"
+        return Promise.reject(refreshError)
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default api;
