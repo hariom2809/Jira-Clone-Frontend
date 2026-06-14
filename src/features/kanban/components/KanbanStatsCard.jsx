9d223f429b2316cc49e524447f1128b70f037137
Guptahariom28 <hariomgupta2809@gmail.com>
2026-06-14 21:03:10 +0530

    kanabn stats card basic done

--- src/features/kanban/components/KanbanStatsCard.jsx
+++ src/features/kanban/components/KanbanStatsCard.jsx
@@ -1,9 +1,15 @@
 import Card from "../../../components/ui/Card"
 
-export default function KanbanStatsCard() {
+export default function KanbanStatsCard({title, value}) {
     return (
         <Card>
-            
+            <h2 className="font-semibold justify-center text-[#F9FAFB] text-3xl">
+                {title}
+            </h2>
+
+            <p className="text-[#9CA3AF] text-3xl justify-center">
+                {value}
+            </p>
         </Card>
     )
 } 
--- src/features/kanban/components/KanbanStstaGrid.jsx
+++ src/features/kanban/components/KanbanStstaGrid.jsx
@@ -1,9 +1,29 @@
 import KanbanStatsCard from "./KanbanStatsCard";
 
-export default function KanbanStatsGrid() {
+export default function KanbanStatsGrid({ issues }) {
+
+    const todoIssues = issues.filter(
+        issue => issue.status === "to_do"
+    ).length
+
+    const inProgessIssues = issues.filter(
+        issue => issue.status === "in_progress"
+    ).length
+
+    const inReviewIssues = issues.filter(
+        issue => issue.status === "in_review"
+    ).length
+
+    const doneIssues = issues.filter(
+        issue => issue.status === "done"
+    ).length
+
     return (
-        <div className="grid gap-4 grid-cols-5">
-            <KanbanStatsCard />
+        <div className="grid gap-4 grid-cols-4">
+            <KanbanStatsCard title="To Do" value={todoIssues} />
+            <KanbanStatsCard title="In Progress" value={inProgessIssues} />
+            <KanbanStatsCard title="In Review" value={inReviewIssues} />
+            <KanbanStatsCard title="Done" value={doneIssues} />
         </div>
     )
 }
